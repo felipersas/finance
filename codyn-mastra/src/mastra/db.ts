c1448362454d6a298d827affceb77a3d56f8b466
Felipe Marques <felipemarques.computacao@gmail.com>
2025-10-18 14:17:14 -0300

    fix: mastra DB

--- codyn-mastra/src/mastra/db.ts
+++ codyn-mastra/src/mastra/db.ts
@@ -4,7 +4,7 @@ export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  connectionTimeoutMillis: 13000, // Return an error after 13 seconds if connection could not be established
 });
 
 // Test connection on initialization (silent unless error)
