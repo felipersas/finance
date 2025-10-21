9384e564d155069966d2728870a4634ace271d2b
Felipe Marques <felipemarques.computacao@gmail.com>
2025-10-21 09:15:54 -0300

    feat: use per-user thread/resource in SQL agent

--- codyn-mastra/src/mastra/agents/sql-agent.ts
+++ codyn-mastra/src/mastra/agents/sql-agent.ts
@@ -2,14 +2,29 @@ import { Agent } from '@mastra/core/agent';
 import { executeSqlQueryTool } from '../tools/execute-sql-query';
 import { openai } from '@ai-sdk/openai';
 import { prompt } from '../prompts/sql-agent';
+import { User } from '../types/user';
 
 export const sqlAgent = new Agent({
   name: 'SQL Financial Assistant',
   instructions: prompt,
   model: openai('gpt-4o'),
-  defaultGenerateOptions: {
-    maxTokens: 4096,
-    temperature: 0.0,
+  defaultGenerateOptions({runtimeContext}) {
+    const user: User = runtimeContext.get("user");
+    return {
+      maxTokens: 4096,
+      temperature: 0.0,
+      threadId: user.sub,
+      resourceId: user.sub,
+    }
+  },
+  defaultStreamOptions({runtimeContext}) {
+    const user: User = runtimeContext.get("user");
+    return {
+      maxTokens: 4096,
+      temperature: 0.0,
+      threadId: user.sub,
+      resourceId: user.sub,
+    };
   },
   tools: {
     executeSqlQuery: executeSqlQueryTool,
