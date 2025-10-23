6e418deaec67ce4210613ae7e138918f52e7662d
Felipe Marques <felipemarques.computacao@gmail.com>
2025-10-22 22:09:53 -0300

    feat: polar

--- finance/apps/web/src/lib/auth-client.ts
+++ finance/apps/web/src/lib/auth-client.ts
@@ -1,8 +1,9 @@
 import type { auth } from "@finance/auth";
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
+import { polarClient } from "@polar-sh/better-auth";
 
 export const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
-	plugins: [inferAdditionalFields<typeof auth>()],
+	plugins: [inferAdditionalFields<typeof auth>(), polarClient()],
 });
